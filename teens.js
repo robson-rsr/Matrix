2a23a4b069091481d14fe870ec7e11ce47dd5092
Robson Silva da Rocha <13.robsonsr@gmail.com>
2023-04-24 19:37:44 -0300

    Projeto Matrix

---
+++ teens.js
@@ -0,0 +1,25 @@
+function logar(){
+    var equipe = document.getElementById('equipe').value;
+    var senha = document.getElementById('senha').value;
+
+    if(equipe == "Preta" && senha == "ALPHA", "ADONAI"){
+        alert("Acesso Permitido");
+        location.href = "matrix.html";
+    }else
+        if(equipe == "Laranja" && senha == "OMEGA", "RAFAH"){
+        alert("Acesso Permitido");
+        location.href = "matrix.html";
+
+    }else
+    if(equipe == "Roxa" && senha == "JIREH", "BETA"){
+    alert("Acesso Permitido");
+    location.href = "matrix.html";
+   
+    }else
+    if(equipe == "Verde" && senha == "GAMA", "ELOHIM"){
+    alert("Acesso Permitido");
+    location.href = "matrix.html";
+    }else
+        alert("Acesso negado")
+    }
+    
